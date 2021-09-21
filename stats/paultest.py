48e8cae709ef954e08994c416dfb99ab3f13fdbe
serrabaum <pmmoody@gmail.com>
2021-09-21 21:06:24 +0100

    Module_2_final

--- stats/paultest.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame({'Date' : ['11/8/2011', '11/9/2011', '11/10/2011','11/11/2011', '11/12/2011'],
-                'Event' : ['Dance', 'Painting', 'Dance', 'Dance', 'Painting']})
-
-df
-
-df.loc[(df.Event == 'Hip-Hop'),'Event']='Dance'
