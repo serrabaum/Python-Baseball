50943c796638e2606c76f3de6137b6fc49fe0948
serrabaum <pmmoody@gmail.com>
2021-09-26 12:04:50 +0100

    Module 4 ongoing

--- stats/pitching.py
+++ stats/pitching.py
@@ -5,6 +5,17 @@ from data import games
 #filter only plays to new df 
 plays = games[games['type']=='play']
 
+#create new strike_outs df 
+strike_outs = plays[plays['event'].str.contains('K')]
+
+#create groupby object (panda series)
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+
+#reset index, converts strike_outs to df and names the new column 
+#containing the no. of strike outs om the game
+strike_outs= strike_outs.reset_index(name='strike_outs')
+print(strike_outs)
+
 
 
 
