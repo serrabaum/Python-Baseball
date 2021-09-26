f2e18ab7c039b8f576390dd0585e209a59f9d759
serrabaum <pmmoody@gmail.com>
2021-09-26 13:20:00 +0100

    4 Final

--- stats/pitching.py
+++ stats/pitching.py
@@ -14,7 +14,16 @@ strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 #reset index, converts strike_outs to df and names the new column 
 #containing the no. of strike outs om the game
 strike_outs= strike_outs.reset_index(name='strike_outs')
-print(strike_outs)
+print(strike_outs.dtypes)
+
+strike_outs = strike_outs.loc[:,['year','strike_outs']].apply(pd.to_numeric)
+print(strike_outs.dtypes)
+
+strike_outs.plot(x='year', y='strike_outs', kind='scatter').legend(['Strike Outs'])
+plt.show()
+
+
+
 
 
 
