d2633e2559b7fcc25e7ab763c185c94390f87a89
serrabaum <pmmoody@gmail.com>
2021-09-29 18:00:29 +0100

    Module5 Wed

--- stats/offense.py
+++ stats/offense.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from games import games
+from data import games
 
 plays = games[games['type']=='play']
+plays.columns =['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
-print(plays.dtypes)
+#print(plays.columns)
+
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)') ]
+
+print(hits.head(8))
 
 
